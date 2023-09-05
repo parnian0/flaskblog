bfe81875dece7ce3d863d6d1f3a8c1185129478e
parnian <p.nazari1102@gmail.com>
2023-09-05 18:25:18 +0330

    added route for info page

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     return'Hello world!'
+@app.route('/info')
+def info():
+    return'this is a website written in flask'
+    
 app.run(debug=True)
